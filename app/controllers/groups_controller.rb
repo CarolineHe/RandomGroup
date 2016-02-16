5fa975f7f5c51839ad547caf89387eefcc37a596
CarolineH <carolineherman@hotmail.fr>
2016-02-16 11:09:14 +0100

    delete modification controller group

--- app/controllers/groups_controller.rb
+++ app/controllers/groups_controller.rb
@@ -71,7 +71,6 @@ class GroupsController < ApplicationController
     # Never trust parameters from the scary internet, only allow the white list through.
     def group_params
       params.require(:group).permit(:name, :salle, :tache)
-      params.require(:person).permit(:name, :email, :tel, :group_id, :sensei)
     end
 
 end
